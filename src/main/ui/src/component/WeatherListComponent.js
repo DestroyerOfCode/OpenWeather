867c11ec5cc0cea5f193fe8ee435bc3538e49440
DestroyerOfCode <marius.babkovic9@gmail.com>
2020-12-05 13:28:56 +0100

    stash treti.

--- src/main/ui/src/component/WeatherListComponent.js
+++ src/main/ui/src/component/WeatherListComponent.js
@@ -6,8 +6,7 @@ class WeatherListComponent extends Component {
         super(props)
         this.state = {
             weathers: [],
-            sortBy: "name",
-            isAscending : true,
+            message: null
         }
         // this.deleteWeatherClicked = this.deleteWeatherClicked.bind(this)
         // this.updateWeatherClicked = this.updateWeatherClicked.bind(this)
@@ -16,107 +15,26 @@ class WeatherListComponent extends Component {
     }
 
     componentDidMount() {
-        this.refreshWeathers(this.state.sortBy, this.state.isAscending);
+        this.refreshCourses();
     }
 
-<<<<<<< Updated upstream
-    refreshWeathers(sortBy, isAscending, filters, filterOperator) {
-        console.log(isAscending + " " +  this.state.isAscending)
-        WeatherService.retrieveAllWeathers(sortBy, isAscending, filters, filterOperator)
-=======
-<<<<<<< Updated upstream
-    refreshCourses() {
-        WeatherService.retrieveAllWeathers()//HARDCODED
-=======
     refreshWeathers(sortBy, isAscending, filters, filterOperator, weathers) {
         console.log(isAscending + " " +  this.state.isAscending)
         // console.log("filters: " + " " +  filters)
 
         WeatherService.retrieveAllWeathers(sortBy, isAscending, filters, filterOperator, weathers)
->>>>>>> Stashed changes
->>>>>>> Stashed changes
             .then(
                 response => {
                     //console.log(response);
                     this.setState({ weathers: response.data })
                 }
-            ).then( r => {
-            this.setState({isAscending: !isAscending})})
+            )
     }
 
     render() {
         console.log('render')
         return (
             <div className="container">
-<<<<<<< Updated upstream
-                <div className="row">
-                    {/* <button className="btn btn-success" onClick={() => this.refreshWeathers()}>Refresh</button> */}
-                    {<textarea placeholder= "Id" onBlur= {event => this.refreshWeathers(this.state.sortBy, this.state.isAscending, {"id": event.target.value}, "eq")}></textarea>}
-                    {<textarea placeholder= "City name" onBlur= {event => this.refreshWeathers(this.state.sortBy, this.state.isAscending, {"name": event.target.value}, "eq")}></textarea>}
-                    {<textarea placeholder= "Country" onBlur= {event => this.refreshWeathers(this.state.sortBy, this.state.isAscending, {"country": event.target.value}, "eq")}></textarea>}
-                    {<textarea placeholder= "Latitude bigger than" onBlur= {event => this.refreshWeathers(this.state.sortBy, this.state.isAscending, {"lat": event.target.value}, "gte")}></textarea>}
-                    {<textarea placeholder= "Latitude smaller than" onBlur= {event => this.refreshWeathers(this.state.sortBy, this.state.isAscending, {"lat": event.target.value}, "lte")}></textarea>}
-                    {<textarea placeholder= "Longitude bigger than" onBlur= {event => this.refreshWeathers(this.state.sortBy, this.state.isAscending, {"lon": event.target.value}, "gte")}></textarea>}
-                    {<textarea placeholder= "Longitude smaller than" onBlur= {event => this.refreshWeathers(this.state.sortBy, this.state.isAscending, {"lon": event.target.value}, "lte")}></textarea>}
-                    {<textarea placeholder= "Humidity bigger than" onBlur= {event => this.refreshWeathers(this.state.sortBy, this.state.isAscending, {"humidity": event.target.value}, "gte")}></textarea>}
-                    {<textarea placeholder= "Humidity smaller than" onBlur= {event => this.refreshWeathers(this.state.sortBy, this.state.isAscending, {"humidity": event.target.value}, "lte")}></textarea>}
-                    {<textarea placeholder= "Feel temperature bigger than" onBlur= {event => this.refreshWeathers(this.state.sortBy, this.state.isAscending, {"feels_like": event.target.value}, "gte")}></textarea>}
-                    {<textarea placeholder= "Feel temperature smaller than" onBlur= {event => this.refreshWeathers(this.state.sortBy, this.state.isAscending, {"feels_like": event.target.value}, "lte")}></textarea>}
-                    {<textarea placeholder= "Temperature bigger than" onBlur= {event => this.refreshWeathers(this.state.sortBy, this.state.isAscending, {"temp": event.target.value}, "gte")}></textarea>}
-                    {<textarea placeholder= "Temperature smaller than" onBlur= {event => this.refreshWeathers(this.state.sortBy, this.state.isAscending, {"temp": event.target.value}, "lte")}></textarea>}
-                    {<textarea placeholder= "Temperature max bigger than" onBlur= {event => this.refreshWeathers(this.state.sortBy, this.state.isAscending, {"temp_max": event.target.value}, "gte")}></textarea>}
-                    {<textarea placeholder= "Temperature max smaller than" onBlur= {event => this.refreshWeathers(this.state.sortBy, this.state.isAscending, {"temp_max": event.target.value}, "lte")}></textarea>}
-                    {<textarea placeholder= "Temperature min bigger than" onBlur= {event => this.refreshWeathers(this.state.sortBy, this.state.isAscending, {"temp_min": event.target.value}, "gte")}></textarea>}
-                    {<textarea placeholder= "Temperature min smaller than" onBlur= {event => this.refreshWeathers(this.state.sortBy, this.state.isAscending, {"temp_min": event.target.value}, "lte")}></textarea>}
-                    {<textarea placeholder= "Description" onBlur= {event => this.refreshWeathers(this.state.sortBy, this.state.isAscending, {"description": event.target.value}, "eq")}></textarea>}
-                </div>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th onClick={() =>this.refreshWeathers("id", this.state.isAscending) }>cityId</th>
-                            <th onClick={() =>this.refreshWeathers("name", this.state.isAscending) }>city Name</th>
-                            <th onClick={() =>this.refreshWeathers("coord.lat", this.state.isAscending) }>latitude</th>
-                            <th onClick={() =>this.refreshWeathers("coord.lon", this.state.isAscending) }>longitude</th>
-                            <th onClick={() =>this.refreshWeathers("sys.country", this.state.isAscending) }>country</th>
-                            <th onClick={() =>this.refreshWeathers("weatherMain.humidity", this.state.isAscending) }>humidity</th>
-                            <th onClick={() =>this.refreshWeathers("weatherMain.feels_like", this.state.isAscending) }>feels like</th>
-                            <th onClick={() =>this.refreshWeathers("weatherMain.temp", this.state.isAscending) }>temperature</th>
-                            <th onClick={() =>this.refreshWeathers("weatherMain.temp_max", this.state.isAscending) }>maximum temperature</th>
-                            <th onClick={() =>this.refreshWeathers("weatherMain.temp_min", this.state.isAscending) }>minimal temperature</th>
-                            <th onClick={() =>this.refreshWeathers("weather.description", this.state.isAscending) }>description</th>                                
-=======
-<<<<<<< Updated upstream
-                <h3>All Weathers</h3>
-                {this.state.message && <div class="alert alert-success">{this.state.message}</div>}
-                <div className="container">
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th>cityId</th>
-                                <th>city Name</th>
-                                {/* <th>Update</th> */}
-                                {/* <th>Delete</th> */}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                this.state.weathers.map(
-                                    weather =>
-                                        <tr key={weather.id}>
-                                            <td>{weather.id}</td>
-                                            <td>{weather.name}</td>
-                                            {/* <td><button className="btn btn-success" onClick={() => this.updateCourseClicked(course.id)}>Update</button></td> */}
-                                            {/* <td><button className="btn btn-warning" onClick={() => this.deleteCourseClicked(course.id)}>Delete</button></td> */}
-                                        </tr>
-                                )
-                            }
-                        </tbody>
-                    </table>
-                    {/* <div className="row"> */}
-                        {/* <button className="btn btn-success" onClick={this.addCourseClicked}>Add</button> */}
-                    {/* </div> */}
-                </div>
-=======
                 <div className="row">
                     {/* <button className="btn btn-success" onClick={() => this.refreshWeathers()}>Refresh</button> */}
                     {<textarea placeholder= "Id" onBlur= {event => this.refreshWeathers(this.state.sortBy, this.state.isAscending, {"id": event.target.value}, "eq", this.state.weathers)}></textarea>}
@@ -152,7 +70,6 @@ class WeatherListComponent extends Component {
                             <th onClick={() =>this.refreshWeathers("temp_max", this.state.isAscending, '', '', this.state.weathers) }>maximum temperature</th>
                             <th onClick={() =>this.refreshWeathers("temp_min", this.state.isAscending, '', '', this.state.weathers) }>minimal temperature</th>
                             <th onClick={() =>this.refreshWeathers("description", this.state.isAscending, '', '', this.state.weathers) }>description</th>                                
->>>>>>> Stashed changes
                             {/* <th>Update</th> */}
                             {/* <th>Delete</th> */}
                         </tr>
@@ -184,10 +101,6 @@ class WeatherListComponent extends Component {
                 {/* <div className="row"> */}
                     {/* <button className="btn btn-success" onClick={this.addCourseClicked}>Add</button> */}
                 {/* </div> */}
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
->>>>>>> Stashed changes
             </div>
         )
     }
--- src/main/ui/src/service/WeatherService.js
+++ src/main/ui/src/service/WeatherService.js
@@ -5,25 +5,6 @@ const WEATHER_API_URL = `${COURSE_API_URL}/weather`
 
 class WeatherService {
 
-<<<<<<< Updated upstream
-    retrieveAllWeathers(sortBy, isAscending, filters, filterOperator) {
-        //console.log('executed service')
-        console.log("Sort: " +sortBy)
-        console.log("filter: " + filters)
-        console.log("filterOperator: " + filterOperator)
-
-        const params = {
-            sortBy, isAscending, filterOperator
-
-        }
-        
-        return axios.post(`${WEATHER_API_URL}/retrieve/fromDb`, filters,{params});
-=======
-<<<<<<< Updated upstream
-    retrieveAllWeathers() {
-        //console.log('executed service')
-        return axios.get(`${WEATHER_API_URL}/retrieve/fromDb`);
-=======
     retrieveAllWeathers(sortBy, isAscending, filters, filterOperator, weathers) {
         //console.log('executed service')
         console.log("Sort: " +sortBy)
@@ -39,8 +20,6 @@ class WeatherService {
         }
         console.log("params: " + JSON.stringify(params) )
         return axios.post(`${WEATHER_API_URL}/retrieve/fromDb`, weathers,{params});
->>>>>>> Stashed changes
->>>>>>> Stashed changes
     }
 }
 export default new WeatherService()
