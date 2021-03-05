989119485ab9449cb197cbd9b224fecd77617a2a
DestroyerOfCode <marius.babkovic9@gmail.com>
2021-03-05 10:04:11 +0100

    added urls for react

--- ui/src/adapters/WeatherCurrentService.js
+++ ui/src/adapters/WeatherCurrentService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const COURSE_API_URL = 'http://localhost:8080'
+const COURSE_API_URL = process.env.REACT_APP_API_URL || process.env.API_URL || 'http://localhost:8080'
 const WEATHER_CURRENT_API_URL = `${COURSE_API_URL}/weather/current`
 
 class WeatherService {
@@ -8,6 +8,7 @@ class WeatherService {
     retrieveAllWeathers(sortBy, isAscending, filters, isFilter, isAdditionalFilter, weathers) {
         console.log("filters: ")
         console.log(filters)
+        console.log(process.env)
         var filterString = this.buildFilterString(filters)
         if(!weathers)
             weathers = []
