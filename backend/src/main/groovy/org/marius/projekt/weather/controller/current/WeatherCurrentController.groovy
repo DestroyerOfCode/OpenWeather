c773a9b98e8063bb7c927f6a2929388efe3bbbe4
DestroyerOfCode <marius.babkovic9@gmail.com>
2021-05-08 15:17:40 +0200

    [FEATURE] deleted redundant files and duplicate src directory and added disfunctional swagger

--- backend/src/main/groovy/org/marius/projekt/weather/controller/current/WeatherCurrentController.groovy
+++ backend/src/main/groovy/org/marius/projekt/weather/controller/current/WeatherCurrentController.groovy
@@ -57,16 +57,19 @@ class WeatherCurrentController {
     }
 
     @Cacheable(value = "currentWeathers")
-    @RequestMapping(method = RequestMethod.POST, value = ["/retrieve/fromDb", "/retrieve/fromDb/{cityId}"])
+    @RequestMapping(method = RequestMethod.POST, value = "/retrieve/fromDb")
     @ResponseBody
-    def getWeatherCurrent(@RequestBody(required = false) Map<String, Object> opts,
-                          @PathVariable(required = false, value = "cityId") String cityId) {
-        PageImpl<WeatherCurrentModel> weathers = weatherService.getWeatherCurrentService(opts, cityId )
+    def getWeatherCurrent(@RequestBody(required = false) Map<String, Object> opts) {
+        PageImpl<WeatherCurrentModel> weathers = weatherService.getWeatherCurrentService(opts)
         if (weathers)
             return new ResponseEntity<>(weathers, HttpStatus.OK)
         return new ResponseEntity<>(weathers, HttpStatus.NO_CONTENT)
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "/test")
+    def getTest(){
+        return 0;
+    }
     @RequestMapping(method = RequestMethod.POST, value = "/save/all")
     @Scheduled(cron = "* 0 * * * ?")
     @ResponseBody
